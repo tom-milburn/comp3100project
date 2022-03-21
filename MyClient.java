c1edd1dab444d0db177f923c4d958436bd925d6b
tom-milburn <tom.milburn@hotmail.com>
2022-03-21 20:20:12 +1100

    test

--- MyClient.java
+++ MyClient.java
@@ -8,19 +8,31 @@ public class MyClient {
         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
         String response = "";
+        String data = "";
+        Job currentJob = new Job();
 
         response = send("HELO", dout, br);
         response = send("AUTH" + System.getProperty("user.name"), dout, br);
         response = send("REDY", dout, br);
 
-        while(!response.equals("NONE")){
+        while(!response.equals("NONE")){ //goto QUIT when recieve NONE
             String[] responseArray = response.split("\s");
-            if(responseArray[0].equals("JOBN")){
-                response = send("GETSCapable "+responseArray[4]+" "+responseArray[5]+" "+responseArray[6], dout, br);
-            } else if(responseArray[0].equals("DATA")){
-                response = send("OK", dout, br);
-                
-            } else break;
+            
+            while(!response.equals(".")){
+                if(responseArray[0].equals("JOBN")){
+                    currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4], responseArray[5], responseArray[6]);
+                    response = send("GETSCapable "+responseArray[4]+" "+responseArray[5]+" "+responseArray[6], dout, br);
+                } else if(responseArray[0].equals("DATA")){
+                    response = send("OK", dout, br);
+                    data = response;
+                } else {
+                    response = send("OK", dout, br);
+                }
+            }
+
+            response = send("SCHD", dout, br); //TODO: finish schd message
+
+            response = send("REDY", dout, br); //client all done, ready for next job
         }
 
         response = send("QUIT", dout, br);
