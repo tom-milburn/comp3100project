29c11e8da06ee3cca380c1a31402bc4d43b3b80d
tom-milburn <tom.milburn@hotmail.com>
2022-03-23 19:35:53 +1100

    client working for a jobs

--- MyClient.java
+++ MyClient.java
@@ -13,32 +13,45 @@ public class MyClient {
         ArrayList<Server> currentServers = new ArrayList<Server>();
         Job currentJob = new Job();
         Server chosenServer = new Server();
+        int numberOfServers = 0, count = 0;
 
         response = send("HELO", dout, br);
-        response = send("AUTH" + System.getProperty("user.name"), dout, br);
+        response = send("AUTH " + System.getProperty("user.name"), dout, br);
         response = send("REDY", dout, br);
 
-        while (!response.equals("NONE")) { // goto QUIT when recieve NONE
-            responseArray = response.split("\s");
-            if (responseArray[0].equals("JOBN")) {
+        while (true) {
+            if(response.contains("NONE")){
+                break;
+            }
+            else if (response.contains("JOBN")) {
+                responseArray = response.split("\s");
                 currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4],
                         responseArray[5], responseArray[6]);
-                response = send("GETSCapable " + currentJob.cores + " " + currentJob.memory + " " + currentJob.disk,
+                response = send("GETS Capable " + currentJob.cores + " " + currentJob.memory + " " + currentJob.disk,
                         dout, br);
-                while(true){
-                    response = send("OK", dout, br);
-                    if(response.equals(".")){
-                        chosenServer = currentServers.get(currentServers.size()-1); //choose last server for now
-                        response = send("SCHD "+currentJob.id+" "+chosenServer.type+" "+chosenServer.id, dout, br);
-                        currentServers.clear(); //remove all servers for next job
-                        break;
-                    }
+                numberOfServers = Integer.parseInt(response.split("\s")[1]);
+                response = send("OK", dout, br);
+                while(count < numberOfServers-1){
                     String[] server = response.split("\s");
-                    currentServers.add(new Server(server[0], server[1], server[3], server[4], server[5], server[6]));
+                    if(server.length>1){
+                        currentServers.add(new Server(server[0], server[1], server[3], server[4], server[5], server[6]));
+                    }
+                    response = br.readLine();
+                    count++;
                 }
+                count = 0;
+                response = send("OK", dout, br);
+                chosenServer = currentServers.get(currentServers.size()-1);
+                currentServers.clear();
+            } 
+            else if(response.contains(".")) {
+                response = send("SCHD "+currentJob.id+" "+chosenServer.type+" "+chosenServer.id, dout, br);
+                response = send("REDY", dout, br);
+            } 
+            else if(response.contains("JCPL")){
+                response = send("REDY", dout, br);
             }
-            break;
-            //response = send("REDY", dout, br); //client all done, ready for next job
+            else break;
         }
 
         response = send("QUIT", dout, br);
@@ -48,10 +61,11 @@ public class MyClient {
 
     }
 
-    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException {
+    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException{
         dout.write((message + "\n").getBytes());
         String response = br.readLine();
-        System.out.println(response);
+        System.out.println("C: "+message);
+        System.out.println("S: "+response);
         return response;
     }
 }
