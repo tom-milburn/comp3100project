fa846827ecf599df1854c63d885aebe6f82b387c
tom-milburn <tom.milburn@hotmail.com>
2022-03-23 17:38:34 +1100

    working for 1 job

--- MyClient.java
+++ MyClient.java
@@ -1,49 +1,55 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class MyClient {
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
         Socket s = new Socket("localhost", 50000);
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
         String response = "";
-        String data = "";
+        String[] responseArray = {};
+        ArrayList<Server> currentServers = new ArrayList<Server>();
         Job currentJob = new Job();
+        Server chosenServer = new Server();
 
         response = send("HELO", dout, br);
         response = send("AUTH" + System.getProperty("user.name"), dout, br);
         response = send("REDY", dout, br);
 
-        while(!response.equals("NONE")){ //goto QUIT when recieve NONE
-            String[] responseArray = response.split("\s");
-            
-            while(!response.equals(".")){
-                if(responseArray[0].equals("JOBN")){
-                    currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4], responseArray[5], responseArray[6]);
-                    response = send("GETSCapable "+responseArray[4]+" "+responseArray[5]+" "+responseArray[6], dout, br);
-                } else if(responseArray[0].equals("DATA")){
+        while (!response.equals("NONE")) { // goto QUIT when recieve NONE
+            responseArray = response.split("\s");
+            if (responseArray[0].equals("JOBN")) {
+                currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4],
+                        responseArray[5], responseArray[6]);
+                response = send("GETSCapable " + currentJob.cores + " " + currentJob.memory + " " + currentJob.disk,
+                        dout, br);
+                while(true){
                     response = send("OK", dout, br);
-                    data = response;
-                } else {
-                    response = send("OK", dout, br);
+                    if(response.equals(".")){
+                        chosenServer = currentServers.get(currentServers.size()-1); //choose last server for now
+                        response = send("SCHD "+currentJob.id+" "+chosenServer.type+" "+chosenServer.id, dout, br);
+                        currentServers.clear(); //remove all servers for next job
+                        break;
+                    }
+                    String[] server = response.split("\s");
+                    currentServers.add(new Server(server[0], server[1], server[3], server[4], server[5], server[6]));
                 }
             }
-
-            response = send("SCHD", dout, br); //TODO: finish schd message
-
-            response = send("REDY", dout, br); //client all done, ready for next job
+            break;
+            //response = send("REDY", dout, br); //client all done, ready for next job
         }
 
         response = send("QUIT", dout, br);
 
         dout.close();
         s.close();
-    
+
     }
 
-    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException{
-        dout.write((message+"\n").getBytes());
+    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException {
+        dout.write((message + "\n").getBytes());
         String response = br.readLine();
         System.out.println(response);
         return response;
--- Server.java
+++ Server.java
@@ -1,23 +1,20 @@
 public class Server {
     String type;
     int id, limit, cores, memory, disk;
-    float hourlyRate;
 
     public Server(){
         type = "undefined";
         id = -1;
         limit = -1;
-        hourlyRate = -1;
         cores = -1;
         memory = -1;
         disk = -1;
     }
 
-    public Server(String type, String id, String limit, String hourlyRate, String cores, String memory, String disk){
+    public Server(String type, String id, String limit, String cores, String memory, String disk){
         this.type = type;
         this.id = Integer.parseInt(id);
         this.limit = Integer.parseInt(limit);
-        this.hourlyRate = Float.parseFloat(hourlyRate);
         this.cores = Integer.parseInt(cores);
         this.memory = Integer.parseInt(memory);
         this.disk = Integer.parseInt(disk);
