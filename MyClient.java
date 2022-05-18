a0d2ccd52bc0cc425ff7a5a7f889f7cdfe0a6f56
tom-milburn <tom.milburn@hotmail.com>
2022-05-18 16:01:44 +1000

    schd algorithm updated

--- MyClient.java
+++ MyClient.java
@@ -9,80 +9,62 @@ public class MyClient {
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-        String response = ""; //store incoming messages from server
+        String response = ""; // store incoming messages from server
         String[] responseArray = {}; // store split server response as components
-        ArrayList<Server> allServers = new ArrayList<Server>(); //store current available servers
-        ArrayList<Server> largestServers = new ArrayList<Server>(); //store only largest servers
-        Job currentJob = new Job(); //store current job
-        Server chosenServer = new Server(); //store server selected for current job
-        int serverCount = 0; //number of servers - used for importing
+        ArrayList<Server> capableServers = new ArrayList<Server>(); // store current capable servers
+        Job currentJob = new Job(); // store current job
+        Server chosenServer = new Server(); // store server selected for current job
+        int serverCount = 0; // number of servers - used for importing
 
-        // hand-shaking 
+        // hand-shaking
         response = send("HELO", dout, br);
         response = send("AUTH " + System.getProperty("user.name"), dout, br);
         response = send("REDY", dout, br);
 
         // main loop - schedule all jobs
         while (true) {
-            if(response.contains("NONE")){ // no more jobs - quit
+            if (response.contains("NONE")) { // no more jobs - quit
                 break;
             }
 
             // recieved job - store job and get capable servers
-            else if (response.contains("JOBN") || response.contains("JOBP")) {
+            if (response.contains("JOBN") || response.contains("JOBP")) {
                 responseArray = response.split("\s");
                 currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4],
                         responseArray[5], responseArray[6]);
-                
-                // get all server data initially
-                if(allServers.isEmpty()){ 
-                    response = send("GETS All", dout, br);
-                    responseArray = response.split("\s");
-                    serverCount=Integer.parseInt(responseArray[1]);
-                    response = send("OK", dout, br); //to recieve all server data
+                response = send("GETS Capable " + currentJob.cores + " " + currentJob.memory + " " + currentJob.disk,
+                        dout, br);
+                responseArray = response.split("\s");
+                serverCount = Integer.parseInt(responseArray[1]);
+                response = send("OK", dout, br); // to recieve all server data
 
-                    while(true){
-                        serverCount--;
-                        String[] server = response.split("\s");
-                        if(server.length>1){ // ensure no more data message '.' is not attempted to be added
-                            allServers.add(new Server(server[0], server[1], server[3], server[4], server[5], server[6]));
-                        }
-                        if(serverCount>0){
-                            response = br.readLine();
-                        }
-                        else break;
+                capableServers.clear();
+                while (true) { // loop for saving server data
+                    serverCount--;
+                    String[] server = response.split("\s");
+                    if (server.length > 1) { // ensure no more data message '.' is not attempted to be added
+                        capableServers.add(new Server(server[0], server[1], server[2], server[3], server[4], server[5], server[6]));
                     }
-                    
-                    response = send("OK", dout, br); //to recieve '.' at end
-
-                    // get all largest servers for LRR
-                    for (Server server : allServers) {
-                        if(largestServers.isEmpty()){
-                            largestServers.add(server);
-                        }
-                        else if(server.cores > largestServers.get(0).cores){
-                            largestServers.clear();
-                            largestServers.add(server);
-                        }
-                        else if(server.cores == largestServers.get(0).cores && server.type.equals(largestServers.get(0).type)){
-                            largestServers.add(server);
-                        }
-                    }
+                    if (serverCount > 0) {
+                        response = br.readLine();
+                    } else
+                        break;
                 }
 
-                // choose server for current job 
-                chosenServer = largestRoundRobin(currentJob, largestServers);
+                response = send("OK", dout, br); // to recieve '.' at end
 
-                //schedule current job and get next job
-                response = send("SCHD "+currentJob.id+" "+chosenServer.type+" "+chosenServer.id, dout, br);
+                chosenServer = selectServer(currentJob, capableServers);
+
+                // schedule current job and get next job
+                response = send("SCHD " + currentJob.id + " " + chosenServer.type + " " + chosenServer.id, dout, br);
                 response = send("REDY", dout, br);
             }
 
-            //recieved completed job or server status info - client still ready
-            else if(response.contains("JCPL") || response.contains("RESF") || response.contains("RESR")){
+            // recieved completed job or server status info - client still ready
+            else if (response.contains("JCPL") || response.contains("RESF") || response.contains("RESR")) {
                 response = send("REDY", dout, br);
-            }
-            else break; // recieved unhandled message from server
+            } else
+                break; // recieved unhandled message from server
         }
 
         // quit server and close connection
@@ -92,17 +74,22 @@ public class MyClient {
 
     }
 
-    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException{
+    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException {
         // handles all sending messages to server and recieveing responces
         dout.write((message + "\n").getBytes());
         String response = br.readLine();
-        System.out.println("C: "+message);
-        System.out.println("S: "+response);
+        System.out.println("C: " + message);
+        System.out.println("S: " + response);
         return response;
     }
 
-    public static Server largestRoundRobin(Job job, ArrayList<Server> servers){
-        // implements 'Largest Round Robin' by scheduling each job on the modulo of its id and the ammount of servers
-        return servers.get(job.id%servers.size());
+    public static Server selectServer(Job job, ArrayList<Server> servers) {
+        //
+        for (Server s : servers) {
+            if(s.status.equals("idle") || s.status.equals("inactive")){
+                return s;
+            }
+        }
+        return servers.get(0);
     }
 }
--- Server.java
+++ Server.java
@@ -1,19 +1,21 @@
 public class Server {
-    String type;
+    String type, status;
     int id, limit, cores, memory, disk;
 
     public Server(){
         type = "undefined";
         id = -1;
+        status = "undefined";
         limit = -1;
         cores = -1;
         memory = -1;
         disk = -1;
     }
 
-    public Server(String type, String id, String limit, String cores, String memory, String disk){
+    public Server(String type, String id, String status, String limit, String cores, String memory, String disk){
         this.type = type;
         this.id = Integer.parseInt(id);
+        this.status = status;
         this.limit = Integer.parseInt(limit);
         this.cores = Integer.parseInt(cores);
         this.memory = Integer.parseInt(memory);
